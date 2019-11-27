6e80f0eecba67d6eaada1c9d0e22e68d00708fbc
David Rhodes <david@davidrhodes.work>
2019-11-26 23:53:05 -0600

    Updates

---
+++ precache-manifest.36dafcc02931c83ad24b6b5a6b49be45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc4a963f96abec3e55713d4511d0573",
+    "url": "/robot-rolodex/index.html"
+  },
+  {
+    "revision": "c5ba857b28c7d3763288",
+    "url": "/robot-rolodex/static/css/main.42965ae3.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/robot-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "c5ba857b28c7d3763288",
+    "url": "/robot-rolodex/static/js/main.a5b16b64.chunk.js"
+  },
+  {
+    "revision": "3aad89ce9cbdbfdeb68f",
+    "url": "/robot-rolodex/static/js/runtime-main.4b32c185.js"
+  }
+]);
